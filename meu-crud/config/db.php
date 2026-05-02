bd960b741c564349b330750921a275c9790e5689
DFN <daviferreiraneris@gmail.com>
2026-05-02 16:30:55 -0300

    Primieira parte

---
+++ meu-crud/config/db.php
@@ -0,0 +1,34 @@
+<?php
+//config/db.php
+function db(): PDO
+{
+$host = '127.0.0.1';
+$dbname ='croud_php';
+$user = 'root';
+$pass = '';// no laragon, frequentemente fica vazio
+
+$dsn = "mysql:host=$host;dbname=$dbname;charset=utf8mb4";
+
+$pdo = new PDO ($dsn, $user, $pass,[
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETECH_MODE=> PDO::FETECH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES=>false,
+
+]);
+return $pdo;
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+?>
---
+++ meu-crud/src/repository.php
@@ -0,0 +1,24 @@
+<?php
+
+// src/repository.php
+require_once_DIR_.'/../config/db.php';
+
+function listar_tarefas(): array
+{
+    $aql ="SELECT" FROM
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
