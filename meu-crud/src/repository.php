f9950289926d4526e62362a6e86b2b87c8e2d6b0
DFN <daviferreiraneris@gmail.com>
2026-05-05 08:10:27 -0300

    continuação

--- meu-crud/src/repository.php
+++ meu-crud/src/repository.php
@@ -5,12 +5,19 @@ require_once_DIR_.'/../config/db.php';
 
 function listar_tarefas(): array
 {
-    $aql ="SELECT" FROM
+    $aql ="SELECT * FROM tarefas ORDER BY id DESC";
+    return db()->query($sql)->fetchAll();
+
 }
 
-
-
-
+function criar_tarefas(string $titulo, ?string $descricao,string $status): int
+{
+    $sql = "INSERT INTO tarfas (título, descricao, status) VALUES(?,?,?)";
+$stmt = db ()-> prepare($sql);
+$stmt->execute([$id]);
+$row = $stmt -> fetch();
+return $row ?:  null;
+}
 
 
 
